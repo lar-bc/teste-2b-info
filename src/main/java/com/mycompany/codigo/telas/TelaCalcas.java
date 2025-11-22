9f7e382dc78009561c437f35547713aff2403890
suzalcantara2009 <suzalcantara2009@gmail.com>
2025-11-22 00:57:35 -0300

    Inserção de botão para carrinho
    (o unico que talvez funcione adicionar no carrinho

--- src/main/java/com/mycompany/codigo/telas/TelaCalcas.java
+++ src/main/java/com/mycompany/codigo/telas/TelaCalcas.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.codigo.telas;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author onesm
@@ -29,7 +31,7 @@ public class TelaCalcas extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        adCJeans = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
@@ -37,89 +39,124 @@ public class TelaCalcas extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
+        adCCargo = new javax.swing.JButton();
+        adCAlfaiataria = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(240, 223, 200));
         jPanel1.setPreferredSize(new java.awt.Dimension(650, 400));
 
-        jLabel1.setText("jLabel1");
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/calça wid leg.png"))); // NOI18N
 
-        jLabel2.setText("jLabel2");
+        jLabel2.setFont(new java.awt.Font("Consola Mono", 0, 12)); // NOI18N
+        jLabel2.setText("R$159,90");
 
-        jButton1.setText("Adicionar ao carrinho");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        adCJeans.setFont(new java.awt.Font("Consola Mono", 0, 10)); // NOI18N
+        adCJeans.setText("Adicionar ao carrinho");
+        adCJeans.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                adCJeansActionPerformed(evt);
             }
         });
 
-        jLabel3.setText("jLabel3");
+        jLabel3.setFont(new java.awt.Font("Consola Mono", 0, 12)); // NOI18N
+        jLabel3.setText("Calça jeans Wide Leg");
 
-        jLabel4.setText("jLabel4");
+        jLabel4.setFont(new java.awt.Font("Consola Mono", 0, 12)); // NOI18N
+        jLabel4.setText("Calça Alfaiataria");
 
-        jLabel5.setText("jLabel5");
+        jLabel5.setFont(new java.awt.Font("Consola Mono", 0, 12)); // NOI18N
+        jLabel5.setText("R$169,90");
 
-        jLabel6.setText("jLabel6");
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/calça de alfaiataria.png"))); // NOI18N
 
-        jLabel7.setText("jLabel7");
+        jLabel7.setFont(new java.awt.Font("Consola Mono", 0, 12)); // NOI18N
+        jLabel7.setText("Calça Cargo Feminina");
 
-        jLabel8.setText("jLabel8");
+        jLabel8.setFont(new java.awt.Font("Consola Mono", 0, 12)); // NOI18N
+        jLabel8.setText("R$149,90");
 
-        jLabel9.setText("jLabel9");
+        jLabel9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/calça cargo.png"))); // NOI18N
+
+        adCCargo.setFont(new java.awt.Font("Consola Mono", 0, 10)); // NOI18N
+        adCCargo.setText("Adicionar ao carrinho");
+        adCCargo.setToolTipText("");
+        adCCargo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                adCCargoActionPerformed(evt);
+            }
+        });
+
+        adCAlfaiataria.setFont(new java.awt.Font("Consola Mono", 0, 10)); // NOI18N
+        adCAlfaiataria.setText("Adicionar ao carrinho");
+        adCAlfaiataria.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                adCAlfaiatariaActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(58, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(146, 146, 146)
-                        .addComponent(jButton1))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(92, 92, 92)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(35, 35, 35))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 176, Short.MAX_VALUE)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel9)
-                                .addGap(159, 159, 159)
-                                .addComponent(jLabel6))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel8)
-                                .addGap(159, 159, 159)
-                                .addComponent(jLabel5))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel7)
-                                .addGap(159, 159, 159)
-                                .addComponent(jLabel4)))))
-                .addGap(112, 112, 112))
+                            .addComponent(adCJeans)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3))
+                        .addGap(30, 30, 30)))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel8)
+                    .addComponent(jLabel7)
+                    .addComponent(jLabel9)
+                    .addComponent(adCCargo))
+                .addGap(26, 26, 26)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel4)
+                    .addComponent(adCAlfaiataria))
+                .addGap(58, 58, 58))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(223, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel7))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel8))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel6)
-                    .addComponent(jLabel9))
-                .addGap(33, 33, 33)
-                .addComponent(jButton1)
-                .addGap(49, 49, 49))
+                .addContainerGap(42, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jLabel9, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel6, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel5))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel7)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel8)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(adCAlfaiataria)
+                            .addComponent(adCCargo)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(adCJeans)))
+                .addGap(71, 71, 71))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -140,9 +177,29 @@ public class TelaCalcas extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void adCJeansActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_adCJeansActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    Item jeans = new Item("Calça Jeans Wide Leg", 159.90);
+    BancoMemorias.carrinho.adicionarItem(jeans);
+
+    JOptionPane.showMessageDialog(this, jeans.getNome() + " adicionado ao carrinho!");
+    }//GEN-LAST:event_adCJeansActionPerformed
+
+    private void adCCargoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_adCCargoActionPerformed
+        // TODO add your handling code here:
+    Item cargo = new Item("Calça Cargo Feminina", 149.90);
+    BancoMemorias.carrinho.adicionarItem(cargo);
+
+    JOptionPane.showMessageDialog(this, cargo.getNome() + " adicionado ao carrinho!");
+    }//GEN-LAST:event_adCCargoActionPerformed
+
+    private void adCAlfaiatariaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_adCAlfaiatariaActionPerformed
+        // TODO add your handling code here:
+    Item alfaiataria = new Item("Calça Alfaiataria", 169.90);
+    BancoMemorias.carrinho.adicionarItem(alfaiataria);
+
+    JOptionPane.showMessageDialog(this, alfaiataria.getNome() + " adicionado ao carrinho!");
+    }//GEN-LAST:event_adCAlfaiatariaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -180,7 +237,9 @@ public class TelaCalcas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton adCAlfaiataria;
+    private javax.swing.JButton adCCargo;
+    private javax.swing.JButton adCJeans;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
